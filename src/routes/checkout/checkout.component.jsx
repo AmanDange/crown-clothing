ea37fd6d40211e4063150060172c645b120b5400
AfrinDange <dangeaman2003@gmail.com>
2024-01-30 23:59:13 +0530

    Stripe Payment included in the code

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,12 @@
 import { useSelector } from 'react-redux';
 
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import { 
+  selectCartItems, 
+  selectCartTotal 
+} from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
@@ -37,7 +41,8 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <Total>Total: Rs.{cartTotal}</Total>
+      <Total>Total: ₹{cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
